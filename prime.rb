f01d4c9ed68b4f5df5074feddfebd819da1fe3f3
EcstasyandVendetta <henkenp125@gmail.com>
2015-10-19 17:19:29 -0400

    Done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+def test(max)
+  primes = (0..max).to_a
+
+primes[0] = primes[1] = nil
+
+count = 0
+primes.each do |p|
+  next unless p
+  break if p*p > max
+  count += 1
+  (p*p).step(max,p) { |m| primes[m] = nil }
+end
+
+  puts "Solved for #{max} in #{count} steps."
+  primes.compact
+end
+
+  def prime?(num)
+    test(num).include?(num)
+  end
